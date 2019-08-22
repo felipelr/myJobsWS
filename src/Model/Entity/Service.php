0acdad3f1b6d5f1c0952bb942d38e9487eb0e7ab
BrunoBersan <brunobersan13@gmail.com>
2019-08-22 15:44:46 -0300

    services

---
+++ src/Model/Entity/Service.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Service Entity
+ *
+ * @property int $id
+ * @property int|null $subcategory_id
+ * @property string|null $description
+ * @property bool|null $active
+ *
+ * @property \App\Model\Entity\Subcategory $subcategory
+ * @property \App\Model\Entity\ProfessionalService[] $professional_services
+ */
+class Service extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'subcategory_id' => true,
+        'description' => true,
+        'active' => true,
+        'subcategory' => true,
+        'professional_services' => true
+    ];
+}
---
+++ tests/Fixture/ServicesFixture.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * ServicesFixture
+ */
+class ServicesFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'subcategory_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'description' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'latin1_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'active' => ['type' => 'boolean', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        '_indexes' => [
+            'subcategories_id_fk' => ['type' => 'index', 'columns' => ['subcategory_id'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+            'subcategories_id_fk' => ['type' => 'foreign', 'columns' => ['subcategory_id'], 'references' => ['subcategories', 'id'], 'update' => 'noAction', 'delete' => 'noAction', 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'subcategory_id' => 1,
+                'description' => 'Lorem ipsum dolor sit amet',
+                'active' => 1
+            ],
+        ];
+        parent::init();
+    }
+}
