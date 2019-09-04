831ddc14d0066cc597c7bbd7439e69b99f639e0f
Felipe Lima <felipe.lima.flr@gmail.com>
2019-09-03 22:16:50 -0300

    Adicinando Controller de Clientes

---
+++ src/Controller/ClientsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Clients Controller
+ *
+ * @property \App\Model\Table\ClientsTable $Clients
+ *
+ * @method \App\Model\Entity\Client[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ClientsController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['Users']
+        ];
+        $clients = $this->paginate($this->Clients);
+
+        $this->set(compact('clients'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Client id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $client = $this->Clients->get($id, [
+            'contain' => ['Users', 'ClientsAddress']
+        ]);
+
+        $this->set('client', $client);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Client id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $client = $this->Clients->get($id, [
+            'contain' => []
+        ]);
+
+        $errorMessage = '';
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $client = $this->Clients->patchEntity($client, $this->request->getData());
+            if (!$this->Clients->save($client)) {
+                //sucesso
+                $errorMessage = '';
+            } else {
+                //erro
+                $errorMessage = 'Não foi possível salvar os dados do cliente.';
+            }
+        }
+
+        if ($errorMessage == '') {
+            $this->set([
+                'client' => $client,
+                '_serialize' => ['client']
+            ]);
+        } else {
+            $this->set([
+                'error' => true,
+                'errorMessage' => $errorMessage,
+                '_serialize' => ['error', 'errorMessage']
+            ]);
+        }
+    }
+}
