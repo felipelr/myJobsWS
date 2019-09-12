820c37926a277bb090ca29690cb59df0c08c4999
BrunoBersan <brunobersan13@gmail.com>
2019-09-11 22:11:37 -0300

    subcategory id

--- src/Controller/SubcategoriesController.php
+++ src/Controller/SubcategoriesController.php
@@ -18,9 +18,10 @@ class SubcategoriesController extends AppController
     {
         $connection = ConnectionManager::get('default');
         $results = $connection->execute("SELECT subcategories.id, subcategories.category_id, 
-            subcategories.description, subcategories.icon, subcategories.active, COUNT(services.id) AS countServices 
+            subcategories.description, subcategories.icon, subcategories.active, COUNT(services.id) AS countServices, COUNT(professional_services.professional_id) as countProfessionals
             FROM subcategories 
-            INNER JOIN services ON (subcategories.id = services.subcategory_id) 
+            LEFT JOIN services ON (subcategories.id = services.subcategory_id)
+            LEFT JOIN professional_services on (professional_services.service_id = services.id) 
             WHERE subcategories.category_id = $idCategoria 
             GROUP BY subcategories.id, subcategories.category_id, subcategories.description, subcategories.icon, subcategories.active")
             ->fetchAll('assoc');
