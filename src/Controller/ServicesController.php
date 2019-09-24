0d867b328ac5548589157f0aa420df069c2ed996
BrunoBersan <brunobersan13@gmail.com>
2019-09-23 21:22:53 -0300

    services

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
 
 class ServicesController extends AppController
 { 
@@ -17,15 +18,19 @@ class ServicesController extends AppController
     }
 
     public function getBySubcategory($idSubcategory = null)
-    {
-        $services = $this->Services->find('all')
-        ->where(['Services.active = ' => 1])
-        ->andWhere(['Services.subcategory_id = ' => $idSubcategory]);
+    { 
+        $connection = ConnectionManager::get('default');
+        $services = $connection->execute(
+        "   SELECT  * FROM services 
+            WHERE subcategory_id = $idSubcategory
+            ORDER BY title ")
+        ->fetchAll('assoc');
 
         $this->set([
             'services' => $services,
             '_serialize' => ['services']
         ]);
+
     }
    
 }
