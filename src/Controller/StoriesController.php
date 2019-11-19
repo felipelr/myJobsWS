2ec27a7602e4958ac4d4d516bc145ced2bd207c0
Felipe Lima <felipe.lima.flr@gmail.com>
2019-11-19 15:32:57 -0300

    Professional Services, Comments, Stories

--- src/Controller/StoriesController.php
+++ src/Controller/StoriesController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
@@ -15,7 +16,9 @@ class StoriesController extends AppController
     public function view($professional_id = null)
     {
         $stories = [];
-        $query = $this->Stories->find('all')
+        $query = $this->Stories->find('all', [
+            'order' => ['Stories.id' => 'DESC']
+        ])
             ->where(['Stories.professional_id = ' => $professional_id]);
 
         foreach ($query as $row) {
@@ -30,16 +33,53 @@ class StoriesController extends AppController
 
     public function add()
     {
-        
+        $errorMessage = '';
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            try {
+                $image = $this->request->getData('image');
+                $story = $this->Stories->newEntity();
+                $story->professional_id =  $this->request->getData('professional_id');
+                $story->description = $this->request->getData('description');
+
+                $base64 = $image;
+                $time = time();
+                $output_file = WWW_ROOT . 'img' . DS . 'story-' . $time . '.jpeg';
+                $dns_path = "http://myjobs.servicos.ws" . DS . 'img' . DS . 'story-' . $time . '.jpeg';
+
+                $ifp = fopen($output_file, 'wb');
+                fwrite($ifp, base64_decode($base64));
+                fclose($ifp);
+                $story->photo = $dns_path;
+
+                if ($this->Stories->save($story)) {
+                    //sucesso                
+                    $errorMessage = '';
+                } else {
+                    //erro
+                    $errorMessage = 'Não foi possível salvar o story.';
+                }
+            } catch (Exception $ex) {
+                $errorMessage = $ex->getMessage();
+            }
+        }
+
+        if ($errorMessage == '') {
+            $this->set([
+                'story' => $story,
+                '_serialize' => ['story']
+            ]);
+        } else {
+            $this->set([
+                'error' => true,
+                'errorMessage' => $errorMessage,
+                '_serialize' => ['error', 'errorMessage']
+            ]);
+        }
     }
-    
+
     public function edit($id = null)
-    {
-        
-    }
-    
+    { }
+
     public function delete($id = null)
-    {
-        
-    }
+    { }
 }
