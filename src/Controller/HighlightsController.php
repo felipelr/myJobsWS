32f0c940d6c03155f2f2bfb62c909e8f617507ce
BrunoBersan <brunobersan13@gmail.com>
2020-01-16 13:10:23 -0300

    services order

--- src/Controller/HighlightsController.php
+++ src/Controller/HighlightsController.php
@@ -50,4 +50,24 @@ class HighlightsController extends AppController
             '_serialize' => ['highlights']
         ]);
     }
+
+    public function highlightsByService($idService = null)
+    {
+        $connection = ConnectionManager::get('default');
+        $results = $connection->execute(
+            "   SELECT p.id, p.name, p.description, count(ps.service_id) as qtdeServices, p.photo as imagem 
+                FROM highlights as h
+                INNER JOIN professionals as p ON(h.professional_id = p.id)
+                INNER JOIN professional_services as ps ON(ps.professional_id = p.id)
+                WHERE p.active = 1 and h.service_id = $idService
+                GROUP BY p.id"
+        )
+            ->fetchAll('assoc');
+
+        $this->set([
+            'highlights' => $results,
+            '_serialize' => ['highlights']
+        ]);
+    }
+
 }
