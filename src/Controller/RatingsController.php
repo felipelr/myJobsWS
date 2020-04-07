95b03bc3b03fd0e3cc2b73e813062c750ef3c811
Felipe Lima <felipe.lima.flr@gmail.com>
2020-04-07 18:52:56 -0300

    Ajuste Navegação

--- src/Controller/RatingsController.php
+++ src/Controller/RatingsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
@@ -13,6 +14,22 @@ use Exception;
  */
 class RatingsController extends AppController
 {
+    public function professional($professional_id)
+    {
+        $rate = [];
+        $query = $this->Ratings->find('all')->where(['Ratings.professional_id = ' => $professional_id]);
+
+        $rate = $query->select([
+            'avg' => $query->func()->avg('rate'),
+            'count' => $query->func()->count('*'),
+        ])->first();
+
+        $this->set([
+            'rate' => $rate,
+            '_serialize' => ['rate']
+        ]);
+    }
+
     public function rate()
     {
         $errorMessage = '';
